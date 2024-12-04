7d587b7e08a0e323ebe43966e7a921879971837e
Yago Alexandre <yago.affonso@tcema.tc.br>
2024-12-04 08:41:33 -0300

    feat: size of short, int, long

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 
 int main()
 {
-    printf("Tamanho de um short em bytes: %d\n", sizeof(int));
+    printf("Tamanho de um short em bytes: %d\n", sizeof(short));
+    printf("Tamanho de um int em bytes: %d\n", sizeof(int));
+    printf("Tamanho de um long em bytes: %d\n", sizeof(long));
     getchar();
     return 0;
 }
